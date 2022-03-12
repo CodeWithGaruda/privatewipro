febc5291170f29507dd2a10e440c404c85b184d7
CWG <CWG@gmail.com>
2022-03-12 09:11:00 +0530

    this is my first commit

---
+++ src/batch28/Main.java
@@ -0,0 +1,15 @@
+package batch28;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is some sample code");
+	}
+
+	public static void swathi() {
+		System.out.println("this is swathi method");
+
+	}
+
+}
